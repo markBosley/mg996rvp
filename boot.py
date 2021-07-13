33ace1e05143352c685e4e5cf9774efb32aa1aaa
markBosley <mark.bosley@brikeda.com>
2021-07-13 10:33:07 -0700

    sg90 servo

--- boot.py
+++ boot.py
@@ -2,13 +2,14 @@ from machine import Pin
 import time
 import utime
 
-pin = Pin(25, Pin.OUT)
+#pin = Pin(25, Pin.OUT)
 
 x=0
 
-while x<6:
-    pin.toggle()
-    time.sleep_ms(1500)
-    x=x+1
+#while x<6:
+    #pin.toggle()
+    #time.sleep_ms(1500)
+    #x=x+1
 
 print("hello. Robot is starting up")
+
