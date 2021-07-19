ca1052ae34aa08c442c403cc31a3bba918bb6a22
markBosley <mark.bosley@brikeda.com>
2021-07-18 20:21:14 -0700

    test tb6612

--- boot.py
+++ boot.py
@@ -1,6 +1,7 @@
 from machine import Pin
 import time
 import utime
+#import tb6612
 
 
 
@@ -9,10 +10,10 @@ pin = Pin(25, Pin.OUT)
 
 x=0
 
-while x<20:
-    pin.toggle()
-    time.sleep_ms(1500)
-    x=x+1
+#while x<20:
+#   pin.toggle()
+#    time.sleep_ms(1500)
+#   x=x+1
 
 print("hello. Robot is starting up")
 
---
+++ tb6612.py
@@ -0,0 +1,19 @@
+from machine import Pin, PWM
+from time import sleep
+
+#Gray wire gpio19 to pwma
+#purple wire gpio18 to AIN2
+#green wire gpio17 to AIN1
+#purple#2 wire gpio16 to stby
+
+pinPWMA = Pin(19, Pin.OUT)
+pinSTBY = Pin(16, Pin.OUT)
+pinAIN2 = Pin(18, Pin.OUT)
+pinAIN1 = Pin(17, Pin.OUT)
+
+pinSTBY.on()
+pinPWMA.on()
+pinAIN2.on()
+pinAIN1.off()
+
+ 
