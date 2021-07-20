1af9d3d0ab29b45d4483522254054d1ae3bc35ab
markBosley <mark.bosley@brikeda.com>
2021-07-19 20:47:42 -0700

    comment out boot

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mg996rvp
 MicroPython Code for Steering Bar Car
+
+Comment out
+ Ctrl + K then press Ctrl + C
+
+uncomment Out code
+    Ctrl + K then press Ctrl + U
--- brianna.py
+++ brianna.py
@@ -3,15 +3,12 @@ import utime
 
 
 
+def Blink():
+    pin = Pin(25, Pin.OUT)
+    x=0
+    while x<5:
+        pin.toggle()
+        utime.sleep_ms(1500)
+        x=x+1
 
-pin = Pin(25, Pin.OUT)
-
-
-x=0
-
-while x<5:
-    pin.toggle()
-    utime.sleep_ms(1500)
-    x=x+1
-
-print("Greetings. Running Brianna script")
+    print("Greetings. Running Brianna script")
