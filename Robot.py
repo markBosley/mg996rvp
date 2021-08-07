245f9809b06e302ea418d3d633e2061ef096a854
markBosley <mark.bosley@brikeda.com>
2021-08-07 11:33:42 -0700

    robot distance

--- Robot.py
+++ Robot.py
@@ -3,6 +3,9 @@ import utime
 import tb6612 as t
 from HC_SR04 import HCSR04
 
+# HC-SR04
+#blue ttrigger pin 16
+#orange echo pin 17
 
 def stop_button_handler(pin):
     print("stop button clicked")  
@@ -21,10 +24,20 @@ start_button=machine.Pin(13,machine.Pin.IN,machine.Pin.PULL_DOWN)
 stop_button.irq(trigger=machine.Pin.IRQ_RISING,handler=stop_button_handler)
 start_button.irq(trigger=machine.Pin.IRQ_RISING,handler=start_button_handler)
 
+def distance():
+    sensor = HCSR04(trigger_pin=16, echo_pin=17,echo_timeout_us=1000000)
+    abort = False
+    while not abort:
+        distance = sensor.distance_cm()
+        print(distance)
+        if distance<10:
+            abort = True
+        utime.sleep(1)
+
 def go():
     t.move(20000)
     while True:
-        sensor = HCSR04(trigger_pin=13, echo_pin=12,echo_timeout_us=1000000)
+        sensor = HCSR04(trigger_pin=16, echo_pin=17,echo_timeout_us=1000000)
         distance = sensor.distance_cm()
         if distance<10:
             print(distance)
