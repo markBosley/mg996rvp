6cd43603cd997312b7afd7cf637e72f505880cf1
markBosley <mark.bosley@brikeda.com>
2021-08-07 12:01:29 -0700

    hc-sr04 range

--- README.md
+++ README.md
@@ -10,7 +10,14 @@ Ctrl + D
 ## uncomment Out code
     Ctrl + K then press Ctrl + U
 
+# HC-SR04
+Range: 400 cm
+The HC-SR04 sensor works best between 2cm – 400 cm (1" - 13ft) within a 30 degree cone, and is accurate to the nearest 0.3cm.
     
-
+# Raspberry Pi Pico Pin Layout
 ![Pico Layout](img/Raspberry-Pi-Pico-Pinout.jpg)
 
+# Servo Wiring
+
+
+![Servo Wiring](img/mg996r.jpg)
--- Robot.py
+++ Robot.py
@@ -1,4 +1,5 @@
 import machine
+from machine import Pin, PWM
 import utime
 import tb6612 as t
 from HC_SR04 import HCSR04
@@ -6,6 +7,14 @@ from HC_SR04 import HCSR04
 # HC-SR04
 #blue ttrigger pin 16
 #orange echo pin 17
+
+# mg996r servo
+# pin 18 for control signal
+servoPin = PWM(Pin(18))
+servoPin.freq(50)
+
+#motor dirver active
+#purple
 
 def stop_button_handler(pin):
     print("stop button clicked")  
@@ -23,6 +32,25 @@ start_button=machine.Pin(13,machine.Pin.IN,machine.Pin.PULL_DOWN)
 
 stop_button.irq(trigger=machine.Pin.IRQ_RISING,handler=stop_button_handler)
 start_button.irq(trigger=machine.Pin.IRQ_RISING,handler=start_button_handler)
+
+def servo(degrees):
+    if degrees > 180: degrees=180
+    if degrees < 0: degrees=0
+    maxDuty=9000
+    minDuty=1000
+    newDuty=minDuty+(maxDuty-minDuty)*(degrees/180)
+    servoPin.duty_u16(int(newDuty))
+
+def steer():
+    for degree in range(0,180,1):
+        servo(degree)
+        utime.sleep(0.001)
+        print("increasing -- "+str(degree))
+
+    for degree in range(180, 0, -1):
+        servo(degree)
+        utime.sleep(0.001)
+        print("decreasing -- "+str(degree))
 
 def distance():
     sensor = HCSR04(trigger_pin=16, echo_pin=17,echo_timeout_us=1000000)
